ce3bdf3b87b9e7a46438e8bcd774a3f4459dafb1
Denis Gheorghe <gheorghe.n.denismarian18@student.rau.ro>
2022-02-11 15:27:57 +0200

    📦 chore: clean up

--- src/Models/Fise_Imprumut.js
+++ src/Models/Fise_Imprumut.js
@@ -1,45 +1,44 @@
-const mongoose = require('mongoose');
-var moment = require('moment');
+const mongoose = require("mongoose");
+var moment = require("moment");
 var now = moment();
 
-const Fisa_ImprumutSchema = new mongoose.Schema({
+const Fisa_ImprumutSchema = new mongoose.Schema(
+  {
     // _id: { type: String },
     // Cod_Lista_Carti: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Cursanti' },
-    Carti: [{
-        Cod_Carte: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'CartiModel' },
-    }],
-    Cod_Cursant: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'CursantiModel' },
+    Carti: [
+      {
+        Cod_Carte: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "CartiModel",
+        },
+      },
+    ],
+    Cod_Cursant: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "CursantiModel",
+    },
     //  Cod_Curs: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'CursuriModel' },
-    Cod_Angajat: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'AngajatiModel' },
-    Data_Imprumut: { type: String, },
+    Cod_Angajat: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "AngajatiModel",
+    },
+    Data_Imprumut: { type: String },
     Data_Retur: { type: String },
-    Returnat: { type: Boolean, default: false }
+    Returnat: { type: Boolean, default: false },
     // Data_Imprumut: { type: Date, default: Date.now() },
     // Data_Retur: { type: Date, default: Date.now() + 14 * 24 * 60 * 60 * 1000 },
-},
-    {
-        collection: 'Fisa_Imprumut'
-    }
+  },
+  {
+    collection: "Fisa_Imprumut",
+  }
 );
 
-const Fisa_ImprumutModel = mongoose.model('Fisa_ImprumutModel', Fisa_ImprumutSchema);
+const Fisa_ImprumutModel = mongoose.model(
+  "Fisa_ImprumutModel",
+  Fisa_ImprumutSchema
+);
 module.exports = Fisa_ImprumutModel;
-
-
-// Fisa_ImprumutSchema.pre('save', beforeSave);
-// Fisa_ImprumutSchema.pre('delete', beforeDelete);
-// Fisa_ImprumutSchema.pre('remove', () => {
-
-// });
-
-// const beforeSave = (x) => {
-//     console.log('I did this beforeSave', x)
-// }
-
-// function beforeSave(x) {
-//     console.log('I did this beforeSave', x)
-// }
-
-// function beforeDelete() {
-//     // ...
-// }
--- src/routes/carti.routes.js
+++ src/routes/carti.routes.js
@@ -1,32 +1,31 @@
 // Routes for carti endpoint
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Logger = require('../logger');
-var cors = require('cors');
+const Logger = require("../logger");
+var cors = require("cors");
 
-const CartiModel = require('../Models/Carti');
+const CartiModel = require("../Models/Carti");
 
-router.post('/create', async (req, res) => {
-    const Test = req.body
-    console.log(Test)
-    const newBook = await CartiModel.create(Test);
-    const response = await CartiModel.findOne(newBook).populate("Autor").populate("Cod_Editura")
-    console.log(response)
-    res.json(response)
-})
+router.post("/create", async (req, res) => {
+  const Test = req.body;
+  console.log(Test);
+  const newBook = await CartiModel.create(Test);
+  const response = await CartiModel.findOne(newBook)
+    .populate("Autor")
+    .populate("Cod_Editura");
+  console.log(response);
+  res.json(response);
+});
 
+router.get("/get", async (req, res) => {
+  const ListaCarti = await CartiModel.find({})
+    .populate("Autor")
+    .populate("Cod_Editura");
 
-
-router.get('/get', async (req, res) => {
-    const ListaCarti = await CartiModel
-        .find({})
-        .populate('Autor')
-        .populate('Cod_Editura')
-
-    res.json(ListaCarti)
-    console.log('Response => ', ListaCarti)
-})
+  res.json(ListaCarti);
+  console.log("Response => ", ListaCarti);
+});
 
 // router.get('/get/:query', cors(), function (req, res) {
 //     var query = req.params.query;
@@ -44,13 +43,14 @@ router.get('/get', async (req, res) => {
 //     })
 // })
 
-router.get('/get/language/:query', cors(), async (req, res) => {
-    var query = req.params.query;
-    const response = await CartiModel.find({ 'Limba': query }).populate("Autor").populate("Cod_Editura")
-    console.log(response)
-    res.json(response)
-})
-
+router.get("/get/language/:query", cors(), async (req, res) => {
+  var query = req.params.query;
+  const response = await CartiModel.find({ Limba: query })
+    .populate("Autor")
+    .populate("Cod_Editura");
+  console.log(response);
+  res.json(response);
+});
 
 // router.get('/get/:id', async (req, res) => {
 //     const ListaQuerryCarti = await CartiModel
@@ -64,33 +64,41 @@ router.get('/get/language/:query', cors(), async (req, res) => {
 //     console.log('Response => ', ListaQuerryCarti)
 // })
 
-router.get('/get/author/:query', async (req, res) => {
-    var query = req.params.query;
+router.get("/get/author/:query", async (req, res) => {
+  var query = req.params.query;
 
-    let response = await CartiModel.find({}).populate("Autor").populate("Cod_Editura")
-    response = response.filter(carte => carte.Autor && carte.Autor.Nume_Autor == query || carte.Autor.Prenume_Autor == query)
-    console.log(response)
-    res.json(response)
-})
+  let response = await CartiModel.find({})
+    .populate("Autor")
+    .populate("Cod_Editura");
+  response = response.filter(
+    (carte) =>
+      (carte.Autor && carte.Autor.Nume_Autor == query) ||
+      carte.Autor.Prenume_Autor == query
+  );
+  console.log(response);
+  res.json(response);
+});
 
+router.put("/update", async (req, res) => {
+  const cartiUpdate = req.body.data;
+  Logger.debug(JSON.stringify(cartiUpdate));
+  const updateResultawait = await CartiModel.updateOne(
+    { _id: cartiUpdate.id },
+    cartiUpdate
+  );
+  console.log(updateResultawait);
+  const response = await CartiModel.findOne({ _id: cartiUpdate.id })
+    .populate("Autor")
+    .populate("Cod_Editura");
+  // Logger.debug(response)
+  res.json(response);
+});
 
-
-router.put('/update', async (req, res) => {
-    const cartiUpdate = req.body.data;
-    Logger.debug(JSON.stringify(cartiUpdate))
-    const updateResultawait = await CartiModel.updateOne({ _id: cartiUpdate.id }, cartiUpdate);
-    console.log(updateResultawait);
-    const response = await CartiModel.findOne({ _id: cartiUpdate.id }).populate("Autor").populate("Cod_Editura")
-    // Logger.debug(response)
-    res.json(response)
-})
-
-
-router.delete('/delete', async (req, res) => {
-    const CartiApi = req.body
-    console.log(`Id-ul cartii este: ${CartiApi._id}`)
-    const response = await CartiModel.deleteOne(CartiApi);
-    res.json({ status: 'ok', response })
-})
+router.delete("/delete", async (req, res) => {
+  const CartiApi = req.body;
+  console.log(`Id-ul cartii este: ${CartiApi._id}`);
+  const response = await CartiModel.deleteOne(CartiApi);
+  res.json({ status: "ok", response });
+});
 
 module.exports = router;
